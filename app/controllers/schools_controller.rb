fe1affc1896078891c80bd215c87561897936751
istafamarshall <istafa.a.marshall@gmail.com>
2023-06-23 13:00:02 -0400

    Create schools controller.

---
+++ app/controllers/schools_controller.rb
@@ -0,0 +1,36 @@
+class SchoolsController < ApplicationController
+
+    def index
+        schools = School.all
+        render json: schools, status: :ok
+    end
+    
+    def show
+        school = School.find(params[:id])
+        render json: school, status: :ok
+    end
+    
+    def create
+        school = School.create!(school_params)
+        render json: school, status: :created
+    end
+    
+    def update
+        school = School.find(params[:id])
+        updated_school = school.update!(school_params)
+        render json: school, status: :accepted
+    end
+    
+    def destroy
+        school = School.find(params[:id])
+        school.destroy
+        head :no_content
+    end
+    
+    private
+    
+    def school_params
+        params.permit(:name, :address)
+    end
+
+end
