57c3ff250b225dcfd399b537621cf06de39bbb8c
istafamarshall <istafa.a.marshall@gmail.com>
2023-06-23 12:50:35 -0400

    Add course enrollment  model.

---
+++ app/models/course_enrollment.rb
@@ -0,0 +1,8 @@
+class CourseEnrollment < ApplicationRecord
+
+    belongs_to :student_class
+    belongs_to :student
+
+    has_many :teachers, through: :student_class
+    
+end
---
+++ db/migrate/20230623164918_create_course_enrollments.rb
@@ -0,0 +1,11 @@
+class CreateCourseEnrollments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :course_enrollments do |t|
+
+      t.integer :student_course_id
+      t.integer :student_id
+
+      t.timestamps
+    end
+  end
+end
