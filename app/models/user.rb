94a2ce46692500760cb1f8b3727606a322660594
istafamarshall <istafa.a.marshall@gmail.com>
2023-06-23 12:37:22 -0400

    Add user model.

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+
+    has_secure_password
+
+    has_many :teachers
+    has_many :schools, through: :teachers
+    has_many :students
+    has_many :student_classes, through: :teachers
+    has_many :course_enrollments, through: :students
+    
+end
