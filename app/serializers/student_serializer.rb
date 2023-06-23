6525e4c35945e2f2b49c10a44e2df8cf82889724
istafamarshall <istafa.a.marshall@gmail.com>
2023-06-23 13:04:41 -0400

    Create student serializer

---
+++ app/serializers/student_serializer.rb
@@ -0,0 +1,16 @@
+class StudentSerializer < ActiveModel::Serializer
+  
+  attributes :id, :name, :address, :email, :birthday
+
+
+  attribute :school do 
+  self.object.school
+  end
+
+  attribute :user do 
+    self.object.user
+  end
+
+  has_many :student_classes, serializer: StudentClassSummarySerializer
+  
+end
