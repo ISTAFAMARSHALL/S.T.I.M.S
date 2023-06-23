fcf9b738fbf8ca0086120ea2ffa64b4e918141ee
istafamarshall <istafa.a.marshall@gmail.com>
2023-06-23 13:09:43 -0400

    Create course enrollment serializer

---
+++ app/serializers/course_enrollment_serializer.rb
@@ -0,0 +1,13 @@
+class CourseEnrollmentSerializer < ActiveModel::Serializer
+  
+  attributes :id, :student_class_id, :student_id
+
+  attribute :student do 
+    self.object.student
+  end
+
+  attribute :teacher do 
+    self.object.teachers
+  end
+  
+end
