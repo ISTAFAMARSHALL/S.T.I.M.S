99353a794e65c248552cb8b8686e0f5da8d97e2f
istafamarshall <istafa.a.marshall@gmail.com>
2023-06-23 13:03:12 -0400

    Create users serializer

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,25 @@
+class UserSerializer < ActiveModel::Serializer
+  attributes :id , :email, :auth_level, :image
+
+  attribute :schools do 
+    self.object.schools
+  end
+
+  # attribute :teachers do 
+  #   self.object.teachers
+  # end
+
+  # attribute :student_classes do 
+  #   self.object.student_classes
+  # end
+
+  # attribute :course_enrollments do 
+  #   self.object.course_enrollments
+  # end
+
+  has_many :teachers
+  has_many :students
+  has_many :student_classes
+  has_many :course_enrollments
+  
+end
