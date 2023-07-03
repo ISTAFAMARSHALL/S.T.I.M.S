4d3e587fdfbaf78d8b39a7e7195955ea2a318089
istafamarshall <istafa.a.marshall@gmail.com>
2023-07-03 15:10:45 -0400

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   
-  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   
   resources :schools
   resources :students
